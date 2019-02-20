6aaafe08c45ef8b281ea356b223f67f26b55fa2d
Taylor Cox <taylorashtoncox@gmail.com>
2019-02-20 09:38:49 -0500

    Adds handler for input change and verifies that it logs change to console

--- src/components/screens/Calc.js
+++ src/components/screens/Calc.js
@@ -1,14 +1,35 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import { View, StyleSheet, TextInput } from 'react-native'
 
 class Calc extends Component {
+    handleInput(text){
+        console.log(text)
+        this.setState({
+            text: text
+        })
+    }
+
     render() {
         return (
-            <View>
-                <Text>Test</Text>
+            <View style={{ flex: 1 }}>
+                <TextInput
+                    onChangeText={(this.handleInput.bind(this))}
+                    value={"360"}
+                    style={styles.input} />
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    input: {
+        height: 150, 
+        width: 100 + '%',
+        backgroundColor: '#272727',
+        color: '#fff',
+        fontSize: 48,
+        textAlign: 'right'
+    },
+  });
 
 export default Calc;
